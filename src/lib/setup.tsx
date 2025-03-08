eeb3bf9fabd66b93aedead1b70b13899ad077aed
Visnu Pitiyanuvath <visnup@gmail.com>
2025-03-07 20:52:52 -0800

    start player setup

--- src/lib/setup.tsx
+++ src/lib/setup.tsx
@@ -1,34 +1,46 @@
-import type { GameObject } from "@tabletop-playground/api";
-import { Color, UIElement, Vector, world } from "@tabletop-playground/api";
+import type { Button, GameObject } from "@tabletop-playground/api";
+import {
+  refPackageId as _refPackageId,
+  Color,
+  UIElement,
+  Vector,
+  world,
+} from "@tabletop-playground/api";
 import { jsxInTTPG, render } from "jsx-in-ttpg";
 
+const refPackageId = _refPackageId;
 const origin = new Vector(0, 0, world.getTableHeight());
 
 type Position = {
   template: string;
+  back?: string;
   color?: Color;
   board?: GameObject;
   removable?: true;
 };
 const positions: Position[] = [
   {
-    template: "80BAFE47F441F064D5A344AEE62E71E6", // bluefin squadron
+    back: "80BAFE47F441F064D5A344AEE62E71E6", // bluefin squadron
+    template: "300F62A0D44DB4F40DA0E4A328CB3758",
     color: new Color(0.0824, 0.1569, 0.298, 1),
   },
   {
-    template: "87F8C9518F4700BB8DAB9CADE119C764", // red smuggler
+    back: "87F8C9518F4700BB8DAB9CADE119C764", // red smuggler
+    template: "7A8D72AAA64A2A976DCE2A9602C41680",
     color: new Color(0.9176, 0.0392, 0.1647, 1),
     removable: true,
   },
   {
-    template: "60466AB1544E5C552EA642AAAE2453E3", // mollusk union
+    back: "60466AB1544E5C552EA642AAAE2453E3", // mollusk union
+    template: "19F163665B477FC3B87512BEC2175203",
     color: new Color(0.9804, 0.8039, 0.0039, 1),
   },
   {
     template: "CD0D5A8D07430CD26FD983914F484812", // fame
   },
   {
-    template: "B9279E8BED4C4A7C660D50BAEC63FC22", // white smuggler
+    back: "B9279E8BED4C4A7C660D50BAEC63FC22", // white smuggler
+    template: "B0DF59A6A844E2CBF4DF44BE8BE35692",
     color: new Color(1, 1, 1, 1),
     removable: true,
   },
@@ -40,19 +52,36 @@ export function initialSetup() {
 
   // Boards
   for (const [i, position] of positions.entries()) {
-    const a = (i * 2 * Math.PI) / positions.length;
+    const a = (i * 2 * Math.PI) / positions.length + Math.PI;
     const p = origin.add([r * Math.cos(a), r * Math.sin(a), 0]);
     const board = (position.board ??= world.createObjectFromTemplate(
-      position.template,
+      position.back ?? position.template,
       p,
     )!);
     board.setPosition(p);
-    board.snapToGround();
     board.setRotation([
       0,
-      (i * 360) / positions.length + ("color" in position ? 180 : 0),
+      (i * 360) / positions.length + ("color" in position ? 0 : 180),
       0,
     ]);
+    board.snapToGround();
+
+    if (!("color" in position)) {
+      const ui = new UIElement();
+      ui.position = new Vector(-8.1, 0, 0.4);
+      ui.scale = 0.2;
+      ui.widget = render(
+        <button
+          size={48}
+          font="Constantia.ttf"
+          fontPackage={refPackageId}
+          onClick={setupPieces}
+        >
+          {" Setup "}
+        </button>,
+      );
+      board.addUI(ui);
+    }
   }
 
   // Slot colors
@@ -69,6 +98,8 @@ export function initialSetup() {
   ui.widget = render(
     <button
       size={48}
+      font="Constantia.ttf"
+      fontPackage={refPackageId}
       onClick={() => {
         positions.splice(
           positions.findIndex((p) => p.board === removable),
@@ -82,4 +113,18 @@ export function initialSetup() {
     </button>,
   );
   removable.addUI(ui);
+}
+
+function setupPieces(button: Button) {
+  for (const p of positions) {
+    if (p.board?.getTemplateId() === p.template) continue;
+    const back = p.board!;
+    const board = (p.board = world.createObjectFromTemplate(
+      p.template,
+      back.getPosition(),
+    )!);
+    board.setRotation(back.getRotation());
+    back.destroy();
+  }
+  button.getOwningObject()?.removeUI(0);
 }
