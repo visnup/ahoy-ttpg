96d4571e810d689bacbbe734a86c8da976557483
Visnu Pitiyanuvath <visnup@gmail.com>
2025-03-07 23:40:45 -0800

    lots of setup

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,43 @@
-import { world } from "@tabletop-playground/api";
+import type { GameObject } from "@tabletop-playground/api";
+import { GameWorld, Vector, world } from "@tabletop-playground/api";
 import { initialSetup } from "./lib/setup";
 
 if (world.getAllObjects().length === 0) initialSetup();
+
+// Extend GameWorld
+declare module "@tabletop-playground/api" {
+  interface GameWorld {
+    getObjectsByTemplateName<T = GameObject>(name: string): T[];
+    getObjectByTemplateName<T = GameObject>(name: string): T | undefined;
+    isOnMap(obj: GameObject): boolean;
+    isOnTable(obj: GameObject, templateNames?: string[]): boolean;
+  }
+}
+GameWorld.prototype.getObjectsByTemplateName = function <T>(name: string) {
+  return this.getAllObjects().filter(
+    (d) => d.getTemplateName() === name,
+  ) as T[];
+};
+GameWorld.prototype.getObjectByTemplateName = function <T>(name: string) {
+  return this.getAllObjects().find((d) => d.getTemplateName() === name) as T;
+};
+
+GameWorld.prototype.isOnMap = function (obj: GameObject) {
+  return this.lineTrace(
+    obj.getPosition(),
+    obj.getPosition().add(new Vector(0, 0, -10)),
+  ).some(({ object }) => object.getTemplateName() === "map");
+};
+GameWorld.prototype.isOnTable = function (
+  obj: GameObject,
+  templateNames: string[] = [],
+) {
+  return this.lineTrace(
+    obj.getPosition(),
+    obj.getPosition().add(new Vector(0, 0, -10)),
+  ).every(
+    ({ object }) =>
+      object.getTemplateName() === obj.getTemplateName() ||
+      templateNames.includes(object.getTemplateName()),
+  );
+};
--- src/lib/setup.tsx
+++ src/lib/setup.tsx
@@ -1,7 +1,9 @@
 import type { Button, GameObject } from "@tabletop-playground/api";
 import {
   refPackageId as _refPackageId,
+  Card,
   Color,
+  Dice,
   UIElement,
   Vector,
   world,
@@ -9,7 +11,7 @@ import {
 import { jsxInTTPG, render } from "jsx-in-ttpg";
 
 const refPackageId = _refPackageId;
-const origin = new Vector(0, 0, world.getTableHeight());
+const origin = new Vector(0, 0, world.getTableHeight() + 0.5);
 
 type Position = {
   template: string;
@@ -66,7 +68,7 @@ export function initialSetup() {
     ]);
     board.snapToGround();
 
-    if (!("color" in position)) {
+    if (!("color" in position) && board.getUIs().length === 0) {
       const ui = new UIElement();
       ui.position = new Vector(-8.1, 0, 0.4);
       ui.scale = 0.2;
@@ -116,6 +118,97 @@ export function initialSetup() {
 }
 
 function setupPieces(button: Button) {
+  const fame = world.getObjectByTemplateName("fame")!;
+
+  // 1. Collect shared pieces
+  const shared = [
+    ["F5DD8208484E7508A7C1BF8743C9DF8D", 20], // gold
+    ["80E8118F324A3F5C2491FFB2447825C4", 25], // damage
+    // ["618F5B66EB4A60801AF271AA0BDFCCF0", 11], // wealth
+    ["48F9260EAF4C1C73134B17AC44CCD1E0", 2], // damage
+  ] as const;
+  const x = fame.getRotation().compose([0, 90, 0]).toVector();
+  const y = fame.getRotation().toVector();
+  const aboveFame = fame
+    .getPosition()
+    .add([0, 0, 1])
+    .add(x.multiply(-6))
+    .add(y.multiply(10));
+  for (const [i, [id, n]] of shared.entries()) {
+    const p = aboveFame.add(x.multiply((i * 12) / (shared.length - 1)));
+    const obj = world.createObjectFromTemplate(id, p)!;
+    obj.setRotation(fame.getRotation());
+    obj.snapToGround();
+    if (obj instanceof Card) {
+      for (let j = 1; j < n; j++)
+        obj.addCards(world.createObjectFromTemplate(id, p) as Card);
+    } else if (obj instanceof Dice) {
+      for (let j = 1; j < n; j++) {
+        const obj = world.createObjectFromTemplate(id, p.add([0, 0, j * 2]))!;
+        obj.setRotation(fame.getRotation());
+        obj.snapToGround();
+      }
+    }
+  }
+
+  // 4. Prepare region stack
+  const regions = world.createObjectFromTemplate(
+    "59DF467DB944A6DB31BDC9BE446B910B",
+    fame.getPosition().add([0, 0, 1]).add(x.multiply(18)),
+  ) as Card;
+  regions.setRotation(fame.getRotation());
+  regions.snapToGround();
+  regions.shuffle();
+  regions.takeCards(1)?.destroy();
+
+  // 5. Set up map
+  const map = regions.takeCards(2)!;
+  map.setPosition(origin);
+  map.snapToGround();
+  process.nextTick(() => map.flipOrUpright());
+  setTimeout(() => {
+    const w = map.getSize().x / 2;
+    const two = map.takeCards(1)!;
+    map.setPosition(origin.add(x.multiply(w + 1e-2)).add(y.multiply(w / 2)));
+    map.setRotation(two.getRotation().compose([0, -90, 0]));
+    two.setPosition(origin.add(x.multiply(-w - 1e-2)).add(y.multiply(-w / 2)));
+    two.setRotation(two.getRotation().compose([0, 90, 0]));
+    for (const o of [map, two]) {
+      o.snapToGround();
+      o.freeze();
+    }
+  }, 1000);
+
+  // 6. Place wealth dice
+
+  // 7. Prepare market
+  const market = world.createObjectFromTemplate(
+    "3A02A16FF6431BC7F78FBB874CC30870",
+    fame.getPosition().add([0, 0, 1]).add(x.multiply(-16)),
+  ) as Card;
+  market.setRotation(fame.getRotation());
+  market.snapToGround();
+  for (let suit = 0; suit < 6; suit++)
+    market
+      .takeCards(1, false, suit * 4 + Math.floor(Math.random() * 5))
+      ?.destroy();
+  market.shuffle();
+
+  // 8. Deal market
+  const belowFame = fame
+    .getPosition()
+    .add([0, 0, 1])
+    .add(x.multiply(-7))
+    .add(y.multiply(-12));
+  for (let i = 0; i < 3; i++) {
+    const card = market.takeCards(1)!;
+    card.setPosition(belowFame.add(x.multiply(i * 7)));
+    process.nextTick(() => card.flipOrUpright());
+  }
+
+  // 9. Mark fame track
+
+  // Flip boards
   for (const p of positions) {
     if (p.board?.getTemplateId() === p.template) continue;
     const back = p.board!;
@@ -126,5 +219,7 @@ function setupPieces(button: Button) {
     board.setRotation(back.getRotation());
     back.destroy();
   }
+
+  // Remove button
   button.getOwningObject()?.removeUI(0);
 }
