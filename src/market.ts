537067c5ea0e041d411dec5ef965a646441456d7
Visnu Pitiyanuvath <visnup@gmail.com>
2025-03-19 23:33:26 -0700

    fix some cargo tucking bugs

--- src/market.ts
+++ src/market.ts
@@ -1,13 +1,8 @@
-import {
-  refCard as _refCard,
-  ObjectType,
-  world,
-} from "@tabletop-playground/api";
+import { refCard as _refCard, world } from "@tabletop-playground/api";
 
 const refCard = _refCard;
 
 if (refCard.getStackSize() === 1) {
-  refCard.onGrab.add((card) => card.setObjectType(ObjectType.Regular));
   refCard.onReleased.add((card) => {
     // Look for a board under the card
     const pc = card.getPosition();
@@ -22,34 +17,37 @@ if (refCard.getStackSize() === 1) {
     )?.object;
     if (!board) return;
 
+    const dz = card.getSize().z;
+
     // Pick the board and everything on it up slightly
     const lift = 1;
     const pb = board.getPosition();
-    const above = new Set(
+    const above = new Map(
       world
         .boxTrace(
-          pb,
+          pb.add([0, 0, -0.3]),
           pb.add([0, 0, lift]),
           board.getExtent(false, true),
           board.getRotation(),
         )
-        .filter((h) => h.object.getTemplateName() !== "market")
-        .map((h) => h.object),
+        .filter((h) => h.object !== card)
+        .map((h) => [h.object, h.object.getObjectType()]),
     );
-    for (const obj of above) {
+    for (const [obj] of above) {
       obj.setPosition(obj.getPosition().add([0, 0, lift]), 1);
       obj.freeze();
     }
 
     // Tuck the card under
     card.setPosition([pc.x, pc.y, board.getPosition().z - lift / 2]);
-    card.setObjectType(ObjectType.Penetrable);
     card.snapToGround();
 
     // Put the board and everything back down
     setTimeout(() => {
-      for (const obj of above)
-        obj.setPosition(obj.getPosition().add([0, 0, -lift]), 1);
+      for (const [obj, type] of above) {
+        obj.setPosition(obj.getPosition().add([0, 0, -lift + dz]), 1);
+        obj.setObjectType(type);
+      }
     }, 200);
   });
 }
